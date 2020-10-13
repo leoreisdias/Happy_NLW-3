efc5a0f07b61a9e40ad5946f046df5173a016ba1
leoreisdias <leoanrdord99@gmail.com>
2020-10-13 19:41:39 -0300

    migrations, images and orphanage tables, and relations oneToMany

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from '../models/Orphanage'
+
+export default {
+    async show(req: Request, res: Response) {
+        const { id } = req.params
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
+
+        return res.json(orphanage);
+    },
+
+    async index(req: Request, res: Response) {
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        });
+
+        return res.json(orphanages);
+    },
+
+    async create(req: Request, res: Response) {
+        const {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        } = req.body;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = req.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
+        const orphanage = orphanagesRepository.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+            images
+        })
+
+        await orphanagesRepository.save(orphanage);
+
+        console.log(orphanage)
+        return res.status(201).json({ message: 'ok' })
+    }
+}
