e1a065da7f5c8466f22d6ebf9576c9205f53d5e3
leoreisdias <leoanrdord99@gmail.com>
2020-10-14 13:14:14 -0300

    link between backend and frontend, multiple files and preview

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,13 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing'
 import OrphanegesMap from './pages/OrphanagesMaps';
+import Orphanege from './pages/Orphanage';
+import CreateOrphanege from './pages/CreateOrphanage';
 
 function Routers() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing} />
-                <Route path="/app" exact component={OrphanegesMap} />
+                <Route path="/app" component={OrphanegesMap} />
+
+                <Route path="/orphanages/create" component={CreateOrphanege} />
+                <Route path="/orphanages/:id" component={Orphanege} />
             </Switch>
         </BrowserRouter>
     );
